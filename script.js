8ba48a55771c13846b70502fa939f840d361eb75
Davide <davidevischetti@gmail.com>
2022-05-19 19:01:11 +0200

    esercizio completo senza stilizzazioni

--- script.js
+++ script.js
@@ -5,14 +5,46 @@ let iEl;
 let h4El;
 let h4UpperText
 
-// RIPETO PER QUANTE VOLTE NECESSARIO
+// CICLO PER RIPETERE LA FUNZIONE DI CREAZIONE HTML
 for (let i = 0; i < myArr.length; i ++) {
     createCardFunc(`${myArr[i].family} ${myArr[i].prefix}${myArr[i].name}`, myArr[i].name, myArr[i].color);
 };
 
-// CREO UN SELECT CHE DIFFERENZI LE VARIE ICONE
+// CREO ARREY DIFFERENZIATI A SECONDA DELLA TIPOLOGIA DELL OBJECT
+const animalsArr = myArr.filter(obj => obj.type == 'animal');
+console.log(animalsArr);
 
+const vegetablesArr = myArr.filter(obj => obj.type == 'vegetable');
+console.log(vegetablesArr);
 
+const usersArr = myArr.filter(obj => obj.type == 'user');
+console.log(usersArr);
+
+// EVENTO PER GESTIRE IL SELECT
+mySelect.addEventListener ('change', function () {
+    if (this.value == 'animals') {
+        myContainer.innerHTML = "";
+        for (let i = 0; i < animalsArr.length; i ++) {
+            createCardFunc(`${animalsArr[i].family} ${animalsArr[i].prefix}${animalsArr[i].name}`, animalsArr[i].name, animalsArr[i].color);
+        };
+    } else if (this.value == 'vegetables') {
+        myContainer.innerHTML = "";
+        for (let i = 0; i < vegetablesArr.length; i ++) {
+            createCardFunc(`${vegetablesArr[i].family} ${vegetablesArr[i].prefix}${vegetablesArr[i].name}`, vegetablesArr[i].name, vegetablesArr[i].color);
+        };
+    } else if (this.value == 'users') {
+        myContainer.innerHTML = "";
+        for (let i = 0; i < usersArr.length; i ++) {
+            createCardFunc(`${usersArr[i].family} ${usersArr[i].prefix}${usersArr[i].name}`, usersArr[i].name, usersArr[i].color);
+        };
+    } else {
+        myContainer.innerHTML = "";
+        for (let i = 0; i < myArr.length; i ++) {
+            createCardFunc(`${myArr[i].family} ${myArr[i].prefix}${myArr[i].name}`, myArr[i].name, myArr[i].color);
+        };
+    };
+
+});
 
 // FUNZIONE PER CREARE LA STRUTTURA HTML
 function createCardFunc (iconClass, h4Text, iconColorClass) {
